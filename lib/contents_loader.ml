f49c1f83a40de1c46114f1f01547917fe0638970
dx3mod <kxyzd@vk.com>
2026-02-08 22:23:45 +0300

    Add contents loader page

---
+++ lib/contents_loader.ml
@@ -0,0 +1,73 @@
+open Containers
+
+exception Load_page_error of string
+
+module Page = struct
+  let datetime_of_yaml = function
+    | `String s ->
+        Date_time.of_string s
+        |> Result.add_ctx "Content_loader"
+        |> Result.map_err (fun msg -> `Msg msg)
+    | _ -> Error (`Msg "invalid date time YAML value")
+
+  type datetime = Date_time.t
+
+  type metadata = {
+    title : string option; [@default None]
+    synopsys : string; [@default ""]
+    publish_date : datetime; [@default Date_time.today ()] [@key "date"]
+  }
+  [@@deriving of_yaml]
+
+  type t = { metadata : metadata; contents : Omd.doc }
+
+  let infer_title doc =
+    match doc with
+    | Omd.Heading (_, 1, Omd.Text (_, title)) :: _ -> Some title
+    | _ -> None
+
+  (** @raise Load_page_error *)
+  let metadata_of_yaml_exn value =
+    metadata_of_yaml value
+    |> Result.get_lazy (fun (`Msg msg) -> raise (Load_page_error msg))
+
+  (** @raise Load_page_error *)
+  let of_string s =
+    let Frontmatter_extractor_yaml.{ attrs; body } =
+      Frontmatter_extractor_yaml.of_string s
+      |> Result.get_lazy (fun (`Msg msg) -> raise (Load_page_error msg))
+    in
+
+    let markdown_contents = Omd.of_string body in
+    let metadata =
+      attrs |> Option.value ~default:(`O []) |> metadata_of_yaml_exn
+    in
+
+    (* inferred title page *)
+    let title =
+      Option.choice [ metadata.title; infer_title markdown_contents ]
+    in
+
+    { contents = markdown_contents; metadata = { metadata with title } }
+
+  let of_channel ic = In_channel.input_all ic |> of_string
+
+  let normalize_links ~root_dir page =
+    let rec normalize_block = function
+      | Omd.Paragraph (attrs, inline) ->
+          Omd.Paragraph (attrs, normalize_inline inline)
+      | block -> block
+    and normalize_inline = function
+      | Omd.Concat (attrs, inlines) ->
+          Omd.Concat (attrs, List.map normalize_inline inlines)
+      | Omd.Link (attrs, link) ->
+          let destination =
+            Fpath.(root_dir // v link.destination |> normalize |> to_string)
+          in
+
+          Omd.Link (attrs, { link with destination })
+      | inline -> inline
+    in
+
+    { page with contents = List.map normalize_block page.contents }
+end
