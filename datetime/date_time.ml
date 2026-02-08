b1a0c1c2f1b3d2a369b8ed3d5cdb53d85f930dd7
dx3mod <kxyzd@vk.com>
2026-02-08 18:45:31 +0300

    Use Container.Parse for parse date time

--- datetime/date_time.ml
+++ datetime/date_time.ml
@@ -1,3 +1,5 @@
+open Containers
+
 type t = { year : int; month : int; day : int }
 
 let ppf fmt { year; month; day } =
@@ -5,11 +7,23 @@ let ppf fmt { year; month; day } =
 
 let pp fmt date = Format.fprintf fmt "Date_time(%a)" ppf date
 
-exception Parse_error of string
-
 let of_string s =
-  try Scanf.sscanf s "%d.%d.%d" @@ fun day month year -> { year; month; day }
-  with Scanf.Scan_failure msg -> raise (Parse_error msg)
+  let open Parse in
+  let number =
+    take1_if is_num >|= Fun.(int_of_string % Parse.Slice.to_string)
+  in
+
+  let make_date_parser del =
+    number <* char del ||| number <* char del ||| number
+  in
+
+  let dd_mm_yy =
+    make_date_parser '.' >|= fun ((day, month), year) -> { day; month; year }
+  and yy_mm_dd =
+    make_date_parser '-' >|= fun ((year, month), day) -> { day; month; year }
+  in
+
+  parse_string (dd_mm_yy <|> yy_mm_dd) s
 
 let compare da db =
   if da.year = db.year && da.month = db.month && da.day = db.day then 0
