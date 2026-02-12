e4847ee2b3627bb208be68af74129b5ffd81ddef
dx3mod <kxyzd@vk.com>
2026-02-12 21:28:26 +0300

    Refactor blog loader functions names

--- lib/blog_loader.ml
+++ lib/blog_loader.ml
@@ -1,6 +1,6 @@
 open Containers
 
-let load_blog_post_page ~project_dir ?posts_dir filename =
+let load_blog_page ~project_dir ?posts_dir filename =
   let project_dir' = Fpath.to_string project_dir in
 
   let filename' =
@@ -17,12 +17,12 @@ let load_blog_post_page ~project_dir ?posts_dir filename =
   |> Blog_page.normalize_links_paths ~project_dir ~posts_dir
   |> Blog_page.with_uri Filename.(concat posts_dir filename)
 
-let load_blog_post_pages ~project_dir posts_dir =
+let load_blog_posts ~project_dir posts_dir =
   let project_dir' = Fpath.to_string project_dir in
 
   Sys.readdir Filename.(concat project_dir' posts_dir)
   |> Array.to_list
-  |> List.map @@ load_blog_post_page ~project_dir ~posts_dir
+  |> List.map @@ load_blog_page ~project_dir ~posts_dir
 
 let load_blog_project_from_dir root_project_dir =
   let root_project_dir' = Fpath.to_string root_project_dir in
@@ -34,12 +34,12 @@ let load_blog_project_from_dir root_project_dir =
   in
 
   let index_page =
-    load_blog_post_page ~project_dir:root_project_dir
+    load_blog_page ~project_dir:root_project_dir
     @@ Filename.concat root_project_dir' "index.md"
   in
 
   let posts =
-    load_blog_post_pages ~project_dir:root_project_dir config.posts_dir
+    load_blog_posts ~project_dir:root_project_dir config.posts_dir
   in
 
   Blog.{ root_dir = root_project_dir; config; index_page; posts }
